6bdb5a2fb43825414655e6df7e2e7793e4f84d8d
Ankit Gupta <ankit9798739110@gmail.com>
2026-07-31 15:21:17 +0530

    initial commit

---
+++ Day01/src/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express();
+require('dotenv').config();
+const main =  require('./config/db')
+const cookieParser =  require('cookie-parser');
+
+app.use(express.json());
+app.use(cookieParser());
+
+main()
+.then(async()=>{
+    app.listen(process.env.PORT,()=>{
+        console.log("Server listening at port number"+process.env.PORT);
+    })
+})
+.cath(err => console.log( "error occured"+err));
---
+++ Day01/src/models/user.js
@@ -0,0 +1,42 @@
+const mongoose=require('mongoose');
+ const {Schema}= mongoose;
+
+ const userSchema= new Schema({
+
+    firstName:{
+        type:String,
+        required:true,
+        minLength =3,
+        maxLength=20
+    },
+    lastName:{
+        type:String,
+        minLength=3,
+        maxLength=20
+    },
+    emailId:{
+        type:String,
+        required:true,
+        unique:true,
+         trim=true,
+         lowercase=true,
+         immutable=true
+    
+    },
+    age:{
+        type=Number,
+        minLength=3,
+        maxLength=3
+
+    },
+     role:{
+        type:String,
+        enum:['user','admin'],
+        default: 'user'
+    },
+    problemSolved:{
+        type:[string]
+    },
+
+
+ })
