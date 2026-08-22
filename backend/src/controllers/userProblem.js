8ed0fa843aa884caec9ac36dde4e4e7483210881
Ankit Gupta <ankit9798739110@gmail.com>
2026-08-22 10:56:43 +0530

    feat(controllers): add submittedProblem controller

--- backend/src/controllers/userProblem.js
+++ backend/src/controllers/userProblem.js
@@ -151,5 +151,26 @@ const getAllProblem = async(req,res)=>{
   }
 }
 
+const submittedProblem = async(req,res)=>{
 
-module.exports = {createProblem,updateProblem,deleteProblem,getProblemById,getAllProblem};
+  try{
+     
+    const userId = req.result._id;
+    const problemId = req.params.pid;
+
+  const ans = await Submission.find({userId,problemId});
+  
+  if(ans.length==0)
+    res.status(200).send("No Submission is persent");
+
+  res.status(200).send(ans);
+
+  }
+  catch(err){
+     res.status(500).send("Internal Server Error");
+  }
+}
+
+
+
+module.exports = {createProblem,updateProblem,deleteProblem,getProblemById,getAllProblem,submittedProblem};
--- backend/src/routes/problemCreator.js
+++ backend/src/routes/problemCreator.js
@@ -3,7 +3,7 @@ const problemRouter=express.Router();
 
 const adminMiddleware=require('../middleware/adminMiddleware');
 const userMiddleware=require('../middleware/userMiddleware');
-const {createProblem,updateProblem,deleteProblem,getProblemById,getAllProblem} = require("../controllers/userProblem");
+const {createProblem,updateProblem,deleteProblem,getProblemById,getAllProblem,submittedProblem} = require("../controllers/userProblem");
 const { getLanguageById } = require('../utils/problemUtility');
 
 
@@ -12,5 +12,7 @@ problemRouter.put("/update/:id",adminMiddleware,updateProblem);
 problemRouter.delete("/delete/:id",adminMiddleware,deleteProblem);
 problemRouter.get("/getproblemById/:id",userMiddleware,getProblemById);
 problemRouter.get("/getAllProblem",userMiddleware,getAllProblem);
+problemRouter.get("submittedProblem/:pid",userMiddleware,submittedProblem);
+
 
 module.exports=problemRouter;
